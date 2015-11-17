e391527769198d5912a3b63bc8cd4e9664e53958
Austin Cathey <thepeoplescoder@gmail.com>
2015-11-17 11:32:37 -0800

    First commit.

---
+++ errorHandler.js
@@ -0,0 +1,28 @@
+// Overriding the default error handler just to show that it can be done.
+// This is to assist in debugging scripts.
+onerror = (function (globalObject, oldOnError)
+{
+	return function (errMsg, scriptUrl, lineNumber, columnNumber, errObj)
+	{
+		// Build error message.
+		var message = "Error triggered.\n\n";
+		message += "Error: " + errMsg + "\n";
+		message += "URL of script: " + scriptUrl + "\n";
+		message += "Line " + lineNumber + ", Column " + columnNumber;
+
+		// Display the message.  We alert() it just in case
+		// if we forget to check the JS console.
+		console.log(message);
+		if (globalObject.alert)
+		{
+			globalObject.alert(message);
+		}
+
+		// Chain previous handler.  Basically "playing nice with others."
+		// Not really necessary here, but I'm just showing that it's possible.
+		if (oldOnError instanceof Function)
+		{
+			oldOnError(errMsg, scriptUrl, lineNumber, columnNumber, errObj);
+		}
+	}
+})(this, onerror);
